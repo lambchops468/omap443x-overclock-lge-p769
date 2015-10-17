92d2fb28615ff6c061f809aecb5c69472082bd0b
Alexander Lam <lambchop468@gmail.com>
2015-10-17 20:43:19 +0000

    suspend_ratelimit: Add new mode with custom wakelock for backing off.
    This adds a wake lock and a new operating mode to the suspend_ratelimit driver.
    This wake lock can be locked for a set amount of time (default 2.5 seconds) upon
    suspend failure.
    
    This is in contrast to the stock suspend_backoff wakelock which would back off
    for 10 seconds and waste more power.
    
    Also, enhance the old backoff mode with being able to accept any number of
    retries.

--- drivers/misc/lg_u2_suspend_ratelimit.c
+++ drivers/misc/lg_u2_suspend_ratelimit.c
@@ -8,22 +8,29 @@
  *
  * When Android performs a suspend, it will try up to 10 times in quick
  * succession when the previous attempt to suspend fails. It seems that this
- * sequence of actions can frequently (up to 5 times a day in New York City)
+ * sequence of actions can frequently (up to 5 times a day with cellular data)
  * cause the SD card's ext4 partition to encounter errors upon failure to
  * suspend. These errors cause the ext4 partition to remount read-only because
  * it is mounted with the mount option errors=remount-ro .
  *
  * The reason for failure to suspend could be anything. The most troublesome
- * one is the cellular radio's baseband processor. When in a celluar dense
- * enviornment like New York City, the baseband processor will interrupt the
- * application CPU very often. This causes suspend attempts to frequently fail.
- * Other causes for suspend failure include Android alarms (set by applications
- * so that they can perform periodic work).
+ * one is the cellular fast dormancy feature. The LG Jellybean implementation of
+ * fast dormancy uses Android Alarms to wake up and check for network traffic.
+ * When there's no network traffic fast dormancy is entered. The alarms cause
+ * suspend attempts to frequently fail because the alarm driver will prevent
+ * suspending when an alarm will go off in the near future.
  *
  * The stock android suspend-retry mechanism in the kernel will try to suspend
  * 10 times following a failure to suspend, and then it will back-off for 10
- * seconds. This driver modifies the variables used for tracking the number
- * of tries and allows no retries before backing off. It also allows one retry.
+ * seconds. This driver has two modes:
+ * - Upon failure to suspend, manipulate the suspend retry counter so only
+ *   a limited number (as low as none) retries are allowed. This relies on the 
+ *   stock back-off mechanism, the suspend_backoff wakelock.
+ * - Upon failure to suspend, lock a wakelock defined by this driver. This lock
+ *   period can be set with the kernel parameter. This allows us to set a
+ *   shorter backoff period lower than 10 seconds so we can attempt to save
+ *   more power. The stock backoff mechanism is unaffected so 10 consecutive
+ *   short suspends will invoke the 10 second backoff.
  *
  * omap_hsmmc_modifier.c was a pervious attempt to solve this problem by
  * keeping the sd card powered up during suspend. It didn't work. It seems like
@@ -36,14 +43,17 @@
 
 #include <linux/string.h>
 #include <linux/suspend.h>
+#include <linux/wakelock.h>
 
 #include "symsearch/symsearch.h"
 
-// If true, allow only one suspend attempt. Otherwise, alow two.
-static bool allow_one_suspend_attempt = true;
-module_param(allow_one_suspend_attempt, bool, S_IRUGO|S_IWUSR);
-MODULE_PARM_DESC(allow_one_suspend_attempt, "If true, only one device suspend "
-	"attempt is allowed before backing off. Otherwise, allow two.");
+// Number of milliseconds to backoff using the custom wakelock.
+static int custom_backoff_msec = 2500;
+module_param(custom_backoff_msec, int, S_IRUGO|S_IWUSR);
+MODULE_PARM_DESC(custom_backoff_msec, "If set >zero, we'll only allow "
+        "one suspend attempt and backoff this many milliseconds. If set to "
+        "<= 0 and > -10, then this is the number of suspend retries to allow "
+        "before invoking the stock 10-second backoff timer.");
 
 /* kernel/power/suspend.c */
 /* This function is not exported so we have to symsearch it */
@@ -56,28 +66,32 @@ static unsigned *suspend_short_count_p = NULL;
 static const struct platform_suspend_ops *omap_pm_ops_p = NULL;
 
 static struct platform_suspend_ops omap_pm_mod_ops;
+static struct wake_lock suspend_ratelimit_lock;
 
 static void suspend_ratelimiter_suspend_recover(void) {
-	if (allow_one_suspend_attempt) {
-		/* Increment suspend_short_count by 9 so that
-		 * wakelock.c's suspend() will backoff after 1 attempt to
-		 * suspend, which failed during device suspend. Note that if an
-		 * early suspend abort occurs (probably because a wakelock is
-		 * held), we still will try to do actual suspend after that, and
-		 * then we will run this code */
-		/* This option appears to be almost 100% stable, but battery
-		 * life is worse (The phone probably spends at least 50% less
-		 * time suspended) */
-		*suspend_short_count_p = 9;
+	if (custom_backoff_msec > 0) {
+	        pr_info("suspend_ratelimit: wakelocking\n");
+		wake_lock_timeout(&suspend_ratelimit_lock,
+				  msecs_to_jiffies(custom_backoff_msec));
 	} else {
-		/* Increment suspend_short_count by 8 so that wakelock.c's
-		 * suspend() will backoff after 2 attempts to suspend, which
-		 * might be caused by failure during device suspend, early
-		 * suspend abort caused by wakelock detect. */
-		/* This option is not quite 100% stable (every few days the SD
-		 * card will have a detection error) */
-		*suspend_short_count_p += 8;
-		if (*suspend_short_count_p > 9) {
+		/* Note: Allowing one suspend attempt and no retries
+		 * (i.e. custom_backoff_msec = 0) seems to be almost 100%
+		 * stable, but battery life is a bit worse. (The phone probably
+		 * spends at least 50% less time suspended).
+		 * Allowing two suspend attemps (one retry) is less stable. The
+		 * SD card will encounter an error every few days.
+		 */
+		int allowed_suspend_retries = -custom_backoff_msec;
+		/* If the suspend_short_count counts the number of retries
+		 * already performed. If it indicates we are out of retries,
+		 * Then set suspend_short_count to 9 to force a backoff.
+		 * Note that suspend_short_count can be incremented without
+		 * invoking this function if suspend fails early due to wakelock
+		 * activity. */
+		if (*suspend_short_count_p >= allowed_suspend_retries) {
+	                pr_info("suspend_ratelimit: clamp suspend retries\n");
+			/* Set to 9 so that wakelock.c's suspend() will backoff
+			 * right away. */
 			*suspend_short_count_p = 9;
 		}
 	}
@@ -87,6 +101,9 @@ static int __init suspend_ratelimiter_init(void) {
 	SYMSEARCH_BIND_FUNCTION_TO(omap_hsmmc_modifier, suspend_set_ops, suspend_set_ops_s);
 	SYMSEARCH_BIND_POINTER_TO(suspend_ratelimiter, struct platform_suspend_ops*, omap_pm_ops, omap_pm_ops_p);
 	SYMSEARCH_BIND_POINTER_TO(suspend_ratelimiter, unsigned*, suspend_short_count, suspend_short_count_p);
+
+        wake_lock_init(&suspend_ratelimit_lock, WAKE_LOCK_SUSPEND,
+                       "suspend_ratelimit");
 
 	omap_pm_mod_ops.begin	= omap_pm_ops_p->begin;
 	omap_pm_mod_ops.end	= omap_pm_ops_p->end;
@@ -103,6 +120,7 @@ static int __init suspend_ratelimiter_init(void) {
 
 static void __exit suspend_ratelimiter_exit(void) {
 	suspend_set_ops_s(omap_pm_ops_p);
+        wake_lock_destroy(&suspend_ratelimit_lock);
 }
 
 module_init(suspend_ratelimiter_init);
